f87d0ccb3448c91f6badb465831d869a388738de
Carolina Sindel Condori Apaza <caroline.sindelc@gmail.com>
2026-07-26 02:00:21 -0400

    Integración módulos

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const {
+    registrarCliente,
+    buscarCliente
+} = require("./clientes");
+
+const {
+    crearCuenta,
+    consultarSaldo
+} = require("./cuentas");
+
+const {
+    depositar,
+    retirar
+} = require("./transacciones");
+
 function sumar(a, b) {
     return a + b;
 }
@@ -6,6 +21,16 @@ function restar(a, b) {
     return a - b;
 }
 
+const cliente = registrarCliente(1, "Carolina");
+const cuenta = crearCuenta("001", cliente.nombre, 1000);
+
+let saldoActual = consultarSaldo("001");
+saldoActual = depositar(saldoActual, 500);
+saldoActual = retirar(saldoActual, 200);
+
+console.log("Cliente:", buscarCliente(1));
+console.log("Cuenta:", cuenta);
+console.log("Saldo final:", saldoActual);
 console.log("Resultado:", sumar(10, 5));
 
 module.exports = {
