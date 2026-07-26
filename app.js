b3873636444cdad70d85d45fa309500083ef636d
Carolina Sindel Condori Apaza <caroline.sindelc@gmail.com>
2026-07-26 01:02:30 -0400

    Agregar prueba de resta

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 function sumar(a, b) {
-  return a + b;
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
 }
 
 console.log("Resultado:", sumar(10, 5));
 
 module.exports = {
-  sumar
+    sumar,
+    restar
 };
--- app.test.js
+++ app.test.js
@@ -1,5 +1,9 @@
-const { sumar } = require('./app');
+const { sumar, restar } = require('./app');
 
 test("Suma correcta", () => {
-    expect(sumar(5,5)).toBe(10);
+    expect(sumar(5, 5)).toBe(10);
+});
+
+test("Resta correcta", () => {
+    expect(restar(8, 3)).toBe(5);
 });
